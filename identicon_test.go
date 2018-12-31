67d246a09683e61c17569c11c89478da02cb2e24
tsdtsdtsd <orkan.alat@googlemail.com>
2018-12-31 01:46:06 +0100

    removed old test

--- identicon_test.go
+++ identicon_test.go
@@ -1,11 +1,8 @@
 package identicon
 
 import (
-	"fmt"
-	"image"
 	"image/color"
 	"image/png"
-	"math"
 	"os"
 	"testing"
 )
@@ -82,35 +79,6 @@ LOOP:
 	}
 }
 
-// func TestColorPalette(t *testing.T) {
-// 	ic, _ := New(id, nil)
-// 	ic.Hash = []byte{
-// 		byte(0),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(1),
-// 		byte(255), // Color byte may not overflow palette size of 215
-// 	}
-
-// 	generated := ic.GenerateImage()
-
-// 	if generated == nil {
-// 		t.Error("Generation for color palette test failed")
-// 	}
-
-// }
-
 func TestOutOfBounds(t *testing.T) {
 	ic, _ := New(id, &Options{
 		BackgroundColor: RGB(235, 235, 235),
@@ -138,23 +106,4 @@ func TestOutOfBounds(t *testing.T) {
 			break
 		}
 	}
-}
-
-func FastCompare(img1, img2 *image.RGBA) (int64, error) {
-	if img1.Bounds() != img2.Bounds() {
-		return 0, fmt.Errorf("image bounds not equal: %+v, %+v", img1.Bounds(), img2.Bounds())
-	}
-
-	accumError := int64(0)
-
-	for i := 0; i < len(img1.Pix); i++ {
-		accumError += int64(sqDiffUInt8(img1.Pix[i], img2.Pix[i]))
-	}
-
-	return int64(math.Sqrt(float64(accumError))), nil
-}
-
-func sqDiffUInt8(x, y uint8) uint64 {
-	d := uint64(x) - uint64(y)
-	return d * d
 }
